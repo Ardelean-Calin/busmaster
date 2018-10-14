d3ae014f18cae201d42d91091a35765c35d2987c
Ardelean Calin <ardelean.calin@outlook.com>
2018-10-14 18:44:30 +0300

    Simplified Opencan DIL interface & Updated lib

--- Sources/BUSMASTER/CAN_OpenCAN/OpenCAN_api.h
+++ Sources/BUSMASTER/CAN_OpenCAN/OpenCAN_api.h
@@ -25,14 +25,18 @@ enum
     CAN_CHANGE_BAUDRATE
 };
 
-/*
- * Function prototypes 
- */
+#ifdef __cplusplus
 extern "C"
 {
+#endif
+    /*
+ * Function prototypes 
+ */
     void *OpenCAN_Open(char *portName);
     void OpenCAN_Close(HANDLE handle);
-    void OpenCAN_WriteCAN(HANDLE hComm, CANMsg_Standard_t *txMsg);
-    uint8_t OpenCAN_ReadCAN(HANDLE hComm, CANMsg_Standard_t *rxMsg);
+    uint8_t OpenCAN_WriteCAN(HANDLE hSerialPort, CANMsg_Standard_t *txMsg);
+    uint8_t OpenCAN_ReadCAN(HANDLE hSerialPort, CANMsg_Standard_t *rxMsg);
+#ifdef __cplusplus
 }
+#endif
 #endif
